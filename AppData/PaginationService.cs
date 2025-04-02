88745b80b994b83941cc244efed13cf30d64cc8e
Набойщиков.Алексей <.@TERM1>
2025-04-02 15:32:14 +0300

    gfgf

--- AppData/PaginationService.cs
+++ AppData/PaginationService.cs
@@ -9,13 +9,19 @@ namespace Bookmaster.AppData
         // Определям поля для хранения данных
         private const int PAGE_SIZE = 50;
         private readonly List<Book> _books;
-        private int _currentPageIndex;
-        private int _currentPageNumber;
+        private int _currentPageIndex = 0;
+        private int _currentPageNumber = 1;
 
         // Определяем свойства для вычисления и хранения данных
 
         public int BooksCount => _books.Count;
         public int TotalPages => (BooksCount + PAGE_SIZE - 1) / PAGE_SIZE;
         public List<Book> CurrentPageOfBooks => _books.Skip(_currentPageIndex * PAGE_SIZE).Take(PAGE_SIZE).ToList();
+
+        // Определение конструктор класса для создания Объекта. В контруктор передаем полный список книг.
+        public PaginationService(List<Book> books)
+        {
+            _books = books;
+        }
     }
 }
--- View/Pages/BrowseCatalogPage.xaml.cs
+++ View/Pages/BrowseCatalogPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Bookmaster.Model;
+﻿using Bookmaster.AppData;
+using Bookmaster.Model;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Bookmaster.View.Pages
@@ -11,6 +13,11 @@ namespace Bookmaster.View.Pages
     public partial class BrowseCatalogPage : Page
     {
         List<Book> _books = App.context.Book.ToList();
+
+        //Создаем объект пагинации
+
+        PaginationService _booksPagination;
+
         public BrowseCatalogPage()
         {
             InitializeComponent();
@@ -19,9 +26,46 @@ namespace Bookmaster.View.Pages
 
         private void SearchBtn_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            BookAuthorLv.ItemsSource = _books.Where(book =>
-            book.Title.ToLower().Contains(SearchByBookTitleTb.Text.ToLower()) &&
-            book.Authors.ToLower().Contains(SearchByAuthorNameTb.Text.ToLower()));
+            if (string.IsNullOrWhiteSpace(SearchByAuthorNameTb.Text) &&
+                string.IsNullOrWhiteSpace(SearchByBookTitleTb.Text))
+            {
+                _booksPagination = new PaginationService(_books);
+
+            }
+
+            else
+            {
+                List<Book> searchResults = _books.Where(book =>
+                book.Title.ToLower().Contains(SearchByBookTitleTb.Text.ToLower()) &&
+                book.Authors.ToLower().Contains(SearchByAuthorNameTb.Text.ToLower())).ToList();
+
+                _booksPagination = new PaginationService(searchResults);
+
+
+            }
+
+            BookAuthorLv.ItemsSource = _booksPagination.CurrentPageOfBooks;
+
+            TotalPagesTbl.Text = _booksPagination.TotalPages.ToString();
+            TotalBooksTbl.Text = _booksPagination.BooksCount.ToString();
+
+            SearchResultsGrid.Visibility = Visibility.Visible;
+
+        }
+
+        private void PreviousBookBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CurrentpageTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NextBookBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
