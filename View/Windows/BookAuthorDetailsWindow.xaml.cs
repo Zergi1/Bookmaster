5d2feb722da6619f07a8c67376568b9d57d5fcf6
Набойщиков.Алексей <.@TERM1>
2025-05-21 16:24:55 +0300

    что то новое

--- View/Windows/BookAuthorDetailsWindow.xaml.cs
+++ View/Windows/BookAuthorDetailsWindow.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bookmaster.Model;
+using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Bookmaster.View.Windows
 {
@@ -19,9 +11,37 @@ namespace Bookmaster.View.Windows
     /// </summary>
     public partial class BookAuthorDetailsWindow : Window
     {
-        public BookAuthorDetailsWindow()
+        public BookAuthorDetailsWindow(Book selectedAuthors)
         {
             InitializeComponent();
+            AuthorsCmb.ItemsSource = selectedAuthors.BookAuthor;
+            Title = $"Авторы книги \" {selectedAuthors.Title}\"";
+        }
+
+        private void AuthorsCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataContext = AuthorsCmb.SelectedItem as BookAuthor;
+
+            Author selectedAuthor = (AuthorsCmb.SelectedItem as BookAuthor).Author;
+
+            OpenWikipediaTbl.Visibility = string.IsNullOrEmpty(selectedAuthor.Wikipedia) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void OpenWikipediaHl_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Close();
+        }
+
+        private void CloseBtn_Click(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            try
+            {
+                Process.Start(e.Uri.AbsoluteUri);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Ошибки", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
